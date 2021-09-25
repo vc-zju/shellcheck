c110a4203f51c5a058ece5ecb542d8a9a5c629d3
Bi Tiekai <52001909+vc-zju@users.noreply.github.com>
2021-09-25 12:13:13 +0800

    Update asd.sh

--- asd.sh
+++ asd.sh
@@ -1,3 +1,18 @@
-#!/usr/bin/env bash
-echo 'hello'
-ababa asd
+#!/bin/sh
+for n in {1..$RANDOM}
+do
+  str=""
+  if (( n % 3 == 0 ))
+  then
+    str="fizz"
+  fi
+  if [ $[n%5] == 0 ]
+  then
+    str="$strbuzz"
+  fi
+  if [[ ! $str ]]
+  then
+    str="$n"
+  fi
+  echo "$str"
+done
